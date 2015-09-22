daee72227513e4059c7ea75b5f480df6a0a65199
Artjoms Porss <artjoms.porss@gmail.com>
2015-09-22 11:45:28 +0100

    code cleanup

--- GuiStudy/src/classes/Table.java
+++ GuiStudy/src/classes/Table.java
@@ -1,17 +1,18 @@
+/*
+* Holds ArrayList with cards on table
+*/
 package classes;
 
 import java.util.ArrayList;
 
 public class Table extends Cards{
 	
-	//CONSTRUCTOR
 	public Table(){
 		super(new ArrayList<Card>());
 	}
 	
 	
-	//INSTANCE METHODS
-	//move cards from table to specified ArrayList
+	//used to move cards from table to specified ArrayList
 	public void moveCardsTo(ArrayList<Card> toPile){
 		while(super.getCards().size() > 0){
 			toPile.add(super.getCards().remove(0));
