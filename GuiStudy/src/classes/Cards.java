5205b814dd9debb80816ccd4434541658e7c7c9e
Artjoms Porss <artjoms.porss@gmail.com>
2015-09-22 10:55:57 +0100

    code cleanup

--- GuiStudy/src/classes/Cards.java
+++ GuiStudy/src/classes/Cards.java
@@ -6,30 +6,29 @@ package classes;
 import java.util.ArrayList;
 
 public class Cards {
-	//VARIABLES
+
 	private ArrayList<Card> cards;
 	
-	//CONSTRUCTOR
+
 	public Cards(){}
 	
 	public Cards(ArrayList<Card> cards){
 		this.cards = cards;
 	}
 	
-	//INSTANCE METHODS
+
 	//returns cards
 	protected ArrayList<Card> getCards(){
 		return cards;
 	}
 	
 	
-	//adds card to cards 
 	protected void addCard(Card c){
 		cards.add(c);
 	}
 	
 	
-	//CLASS METHODS
+	//TESTING
 //	public static void moveFromTo(ArrayList<Card> from, ArrayList<Card> to){
 //		for(Card c : from){
 //			to.add(from.remove(0));
